341a10d7121991bc14818cb9666ccd46812d5a80
DanilWeda <draganevr@gmail.com>
2022-07-18 20:53:25 +0300

    fix empty home page

--- src/components/Planes/index.jsx
+++ src/components/Planes/index.jsx
@@ -4,6 +4,11 @@ import { useEffect, useState } from 'react'
 import { getPlanes } from '../../store/planes/action'
 import CircularProgress from '@mui/material/CircularProgress';
 import PlaneItem from '../PlaneItem'
+import { Typography } from '@mui/material';
+import { paths } from '../../routes/Routes';
+import { Link } from 'react-router-dom'
+import Button from '@mui/material/Button';
+import { Box } from '@mui/system';
 
 const Planes = () => {
 	const dispatch = useDispatch()
@@ -35,6 +40,14 @@ const Planes = () => {
 			{isLoading && <CircularProgress color="success" size={120} sx={{ margin: '0 auto', marginTop: '50vh' }} />}
 			{isError && <h1>{message}</h1>}
 			{serchedPlanes && serchedPlanes.map(plane => (<PlaneItem key={plane._id} {...plane} />))}
+			{planes && !planes.length &&
+				<Box sx={{ display: 'flex', margin: '0 auto', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+					<Typography variant='h3' component='h1' sx={{ with: '100%' }}>Sry, here we don`t have posts</Typography>
+					<Link to={paths.addPlanePage} style={{ textDecoration: 'none', color: 'black', marginTop: '2rem' }}>
+						<Button sx={{ p: '0.9rem', mr: '2rem' }} color="inherit" variant="outlined">Add Air Plane</Button>
+					</Link>
+				</Box>
+			}
 		</ContentWrapper>
 	)
 }
